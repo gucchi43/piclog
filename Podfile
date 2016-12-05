3a528ebe2bd1c2d1cb6b5d8e42f64ba2ede866e4
Hiroki Taniguchi <acmikn43@gmail.com>
2016-12-05 14:20:11 +0900

    piclog大体の構造完成

---
+++ Podfile
@@ -0,0 +1,25 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'picLog' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for picLog
+
+  pod 'Colours'
+  pod 'IDMPhotoBrowser'
+  pod 'Firebase/Core'
+  pod 'Firebase/AdMob'
+
+  target 'picLogTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'picLogUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
---
+++ picLog/LodingViewController.swift
@@ -0,0 +1,35 @@
+//
+//  LodingViewController.swift
+//  picLog
+//
+//  Created by HIroki Taniguti on 2016/11/03.
+//  Copyright © 2016年 HIroki Taniguti. All rights reserved.
+//
+
+import UIKit
+
+class LodingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
